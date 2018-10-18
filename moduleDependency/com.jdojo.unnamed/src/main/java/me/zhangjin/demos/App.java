0aea84b904dc30817fd18c78983537bd49c1be3c
张进 <jinzhanga@Ctrip.com>
2018-10-18 19:42:58 +0800

    cc

--- moduleDependency/com.jdojo.unnamed/src/main/java/me/zhangjin/demos/App.java
+++ moduleDependency/com.jdojo.unnamed/src/main/java/me/zhangjin/demos/App.java
@@ -7,8 +7,9 @@ import com.jdojo.reflect.Item;
  */
 public class App {
     public static void main(String[] args) {
+        // 这不是一个模块化项目（没有 module-info.java）
         // 未命名模块读取普通模块
-        // 在 模块路径 中添加 com.jdojo.reflect
+        // 在“模块路径”中添加 com.jdojo.reflect
         int v = Item.v;
         System.out.println("Item.v = " + v);
     }
