8e6bf3cfe5ff627a101ba62b1a0c3c3aeddaecd7
张进 <jinzhanga@Ctrip.com>
2018-10-19 17:23:59 +0800

    jmod

--- EncapsulationModule/run.bat
+++ EncapsulationModule/run.bat
@@ -1,6 +1,5 @@
 @echo off
 set PATH="%JAVA11_HOME%\bin";%PATH%
-
 goto jp
 
 -C： 表示改变目录
@@ -9,6 +8,10 @@ goto jp
 
 两个包中的公共类一定要一样
 不能是 JDK8Main 和 JDK11Main
+
+--hash-modules <regex-pattern>      
+计算和记录散列, 以将打包模块绑定到与指定 <正则表达式模式> 匹配并直接或间接依赖于的模块。
+散列记录在所创建的 JMOD 文件中, 或者记录在 jmod hash 命令指定的模块路径的 JMOD 文件或模块化 JAR
 
 :jp
 
@@ -43,6 +46,31 @@ jar --create --verbose --file mrjars\com.jdojo.mrjar2.jar -C com.jdojo.mrjar.jdk
 @echo.
 @echo ----- JDK11 运行（使用 JDK8 Main） -----
 java --module-path mrjars\com.jdojo.mrjar2.jar --module com.jdojo.mrjar/com.jdojo.mrjar.Main
+
+@echo.
+@echo ----- 创建 JMOD -----
+jmod create --class-path lib\com.jdojo.prime.jar jmods\com.jdojo.prime.jmod
+
+@echo.
+@echo ----- 提取 JMOD 文件 -----
+jmod extract --dir extracted jmods\com.jdojo.prime.jmod
+
+@echo.
+@echo ----- 列出 JMOD 文件 -----
+jmod list jmods\com.jdojo.prime.jmod
+
+jmod create --class-path lib\com.jdojo.prime.faster.jar jmods\com.jdojo.prime.faster.jmod
+jmod create --class-path lib\com.jdojo.prime.generic.jar jmods\com.jdojo.prime.generic.jmod
+jmod create --class-path lib\com.jdojo.prime.probable.jar jmods\com.jdojo.prime.probable.jmod
+
+@echo.
+@echo ----- 记录散列 -----
+jmod hash --module-path jmods --hash-modules com.jdojo.prime.? 
+jmod describe jmods\com.jdojo.prime.jmod
+
+@echo.
+@echo ----- 验证散列 -----
+jmod hash --dry-run --module-path jmods --hash-modules com.jdojo.prime.? 
 
 @echo.
 cmd
