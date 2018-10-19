23214e7d4924c1e1a6b659efc21bf8ae9bcd5a85
张进 <jinzhanga@Ctrip.com>
2018-10-19 12:38:21 +0800

    maven

--- moduleDependency/com.jdojo.unnamed.test/src/main/java/com/jdojo/unnamed/test/UnnamedTestMain.java
+++ moduleDependency/com.jdojo.unnamed.test/src/main/java/com/jdojo/unnamed/test/UnnamedTestMain.java
@@ -8,7 +8,7 @@ public class UnnamedTestMain {
         // 这是一个 模块化 项目
         // 引用“标准类路径”中的 jar 包
 
-        // 设置 JVM 参数，应用类路径文件
+        // 在配置中，设置 JVM 参数，应用类路径文件
         // --class-path D:\JavaLibs\com.jdojo.reflect-0.0.1-SNAPSHOT.jar
 
         Class<?> cls = Class.forName("com.jdojo.reflect.Item");
--- moduleDependency/com.jdojo.unnamed/src/main/java/me/zhangjin/demos/App.java
+++ moduleDependency/com.jdojo.unnamed/src/main/java/me/zhangjin/demos/App.java
@@ -8,8 +8,11 @@ import com.jdojo.reflect.Item;
 public class App {
     public static void main(String[] args) {
         // 这不是一个模块化项目（没有 module-info.java）
+        // 通过标准的类路径加载模块（运行参数中包含 -classpath）
         // 未命名模块读取普通模块
         // 在“模块路径”中添加 com.jdojo.reflect
+        // 使用 -p / --module-path 参数指定模块路径
+        // 使用 -m / --module <模块>/<主类> 形式执行
         int v = Item.v;
         System.out.println("Item.v = " + v);
     }
